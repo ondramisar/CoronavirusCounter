f24d350bc038c6a096a8f54c6bc8d786e791390d
ondramisar <ondramisar124@seznam.cz>
2020-08-05 22:48:32 +0200

    Add reloading for style sheet

--- src/main/kotlin/MyApp.kt
+++ src/main/kotlin/MyApp.kt
@@ -1,6 +1,7 @@
 import javafx.stage.Stage
 import tornadofx.App
 import tornadofx.Rest
+import tornadofx.reloadStylesheetsOnFocus
 
 class MyApp : App(MainView::class, Styles::class) {
     private val api: Rest by inject()
@@ -8,11 +9,11 @@ class MyApp : App(MainView::class, Styles::class) {
 
     init {
         api.baseURI = "https://disease.sh/v3/covid-19"
+        reloadStylesheetsOnFocus()
     }
 
     override fun start(stage: Stage) {
-       // stage.isResizable = false
-      //  stage.isMaximized = true
+        //  stage.isMaximized = true
         super.start(stage)
     }
 }
